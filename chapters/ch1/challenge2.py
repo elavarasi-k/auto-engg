d54fa51f2fc6f10447016814d33c35f1edd0d702
lvj1kor <kasirajan.elavarasi@bosch.com>
2024-11-27 16:29:17 +0530

    challenge2

---
+++ chapters/ch1/challenge2.py
@@ -0,0 +1,4 @@
+season = input("Provide the current season of the year: ")
+num = input("Enter a number: ")
+desc = input("Add an adjective: ")
+print(f"On a {desc} {season} day, I drink at least {num} cup(s) of juice!")
